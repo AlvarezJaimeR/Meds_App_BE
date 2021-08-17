24ad7663adcda8a8c60a777c510b3594145982e0
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-08-17 12:46:57 -0400

    got server to start

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 const connectDB = require("./startup/db");
 const express = require("express");
 const app = express();
-const users = require("./routes/users");
+// const users = require("./routes/users");
 const cors = require("cors");
-const auth = require("./routes/auth");
+// const auth = require("./routes/auth");
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/users", users);
-app.use("/api/auth", auth);
+// app.use("/api/users", users);
+// app.use("/api/auth", auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
