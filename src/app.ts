cef791cdc850a7a6be342182b1be26f591e821f8
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-08-21 14:49:42 -0400

    added the user model, routes, and middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 const connectDB = require("./startup/db");
 const express = require("express");
 const app = express();
-// const users = require("./routes/users");
+const users = require("./routes/users");
 const cors = require("cors");
 // const auth = require("./routes/auth");
 
@@ -9,7 +9,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/users", users);
+app.use("/api/users", users);
 // app.use("/api/auth", auth);
 
 const port = process.env.PORT || 5000;
