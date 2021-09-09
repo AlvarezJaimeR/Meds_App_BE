6bd12b9c7a72dcb105df3da97c9e014510575ee4
Jaime Alvarez <jrralvarez@gmail.com>
2021-09-08 22:31:02 -0400

    added a regular js routes folder for users.

---
+++ src/jsroutes/users.js
@@ -0,0 +1,18 @@
+const { User } = require("../jsmodels/user");
+const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
+const express = require("express");
+const { Prescription } = require("../jsmodels/prescription");
+const router = express.Router();
+
+//get all users
+router.get("/", async (req, res) => {
+    try {
+      const users = await User.find();
+      return res.send(users);
+    } catch (ex) {
+      return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+module.exports = router;
